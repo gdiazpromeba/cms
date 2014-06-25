5d5c529ac125664e23a2774ec6d893ead8c1781a
Gonzalo Díaz <gdiaz@starmount.com>
2014-06-25 16:45:53 -0300

    redimensionamiento de imágenes

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 
   //servidor qa
-    $GLOBALS['debug'] = false;
-    global $raiz; $raiz = '/home1/petzynga/public_html/qa/qacms';
-    global $usuario; $usuario ='petzynga_gonzalo';
-    global $clave; $clave ='manuela';
-    global $baseDeDatos; $baseDeDatos ='petzynga_cmsqa';
-    $GLOBALS['dirAplicacion'] ='/qa/qacms';
+//     $GLOBALS['debug'] = false;
+//     global $raiz; $raiz = '/home1/petzynga/public_html/qa/qacms';
+//     global $usuario; $usuario ='petzynga_gonzalo';
+//     global $clave; $clave ='manuela';
+//     global $baseDeDatos; $baseDeDatos ='petzynga_cmsqa';
+//     $GLOBALS['dirAplicacion'] ='/qa/qacms';
 
   //local
-//  $GLOBALS['debug'] = true;
-//  global $raiz; $raiz = 'C:/xampp/htdocs/petzynga/cms';
-//  global $usuario; $usuario ='almarlam_gonzalo';
-//  global $clave; $clave ='manuela';
-//  global $baseDeDatos; $baseDeDatos ='petzynga';
-//  $GLOBALS['dirAplicacion'] ='/petzynga';
+ $GLOBALS['debug'] = true;
+ global $raiz; $raiz = 'C:/xampp/htdocs/petzyngacms';
+ global $usuario; $usuario ='almarlam_gonzalo';
+ global $clave; $clave ='manuela';
+ global $baseDeDatos; $baseDeDatos ='petzynga';
+ $GLOBALS['dirAplicacion'] ='/petzyngacms';
     
 ?>
