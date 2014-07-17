70f3d6ffefe70a5f39c49a3cb177c2e2bfefecfc
Don Gonzalito <DonGonzalito@gmail.com>
2014-07-16 23:37:47 -0300

    quita de config

--- oad/impl/ZipsUsaOadImpl.php
+++ oad/impl/ZipsUsaOadImpl.php
@@ -1,6 +1,5 @@
 <?php 
 
-require_once '../../config.php';  
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/oad/AOD.php';  
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/oad/ZipsUsaOad.php';  
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/beans/ZipUsa.php';  
