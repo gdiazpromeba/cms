cfb4ec58ba14475859bcb7b18741f8de2a2224d8
Don Gonzalito <DonGonzalito@gmail.com>
2014-09-11 23:07:10 -0300

    generación del sitemap.xml

--- util/generaSiteMap.php
+++ util/generaSiteMap.php
@@ -4,8 +4,12 @@ require_once '../config.php';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/beans/DogBreed.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/DogBreedsSvcImpl.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/beans/ShelterUsa.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersUsaSvcImpl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersJapanSvcImpl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersCanadaSvcImpl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersUkSvcImpl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersIndiaSvcImpl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirAplicacion'] . '/svc/impl/SheltersChinaSvcImpl.php';
 
 
 
@@ -29,12 +33,13 @@ $res =  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>   \n";
 $res .= "  <?xml-stylesheet type=\"text/xsl\" href=\"gss.xsl\"?>   \n";
 $res .= "  <urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.google.com/schemas/sitemap/0.84 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">   \n";
 
-$rootUrl = "http://petzynga.com/";
-$lastMod = "2014-09-11T19:42:35+00:00";
+$rootUrl = "http://petzynga.com/qaweb/";
+$lastMod = date("Y-m-d");;
 
 $url= $rootUrl;
 
 // main pages
+$res .= "<!-- Main pages  -->   \n";
 $res .= construyeUnidad($url, $lastMod, "weekly", 1);
 $url= $rootUrl . "dogbreeds";
 $res .= construyeUnidad($url, $lastMod, "weekly", 1);
@@ -43,6 +48,7 @@ $res .= construyeUnidad($url, $lastMod, "yearly", 1);
 
 // secondary pages
 //   shelters' countries 
+$res .= "<!-- Initial dog shelters of every country listing  -->   \n";
 $url= $rootUrl . "shelters/listing/usa/initial";
 $res .= construyeUnidad($url, $lastMod, "weekly", 0.5);
 $url= $rootUrl . "shelters/listing/uk/initial";
@@ -61,12 +67,65 @@ $res .= construyeUnidad($url, $lastMod, "weekly", 0.5);
 
 
 //páginas individuales de detalle de shelter
+//  usa
+$res .= "<!-- USA shelters listing   -->   \n";
 $svc = new SheltersUsaSvcImpl();
 $beans=$svc->selTodos(null, null, 0, 0, null, null, 0, 100000);
 foreach ($beans as $bean){
 	$url= $rootUrl . "shelters/info/usa/" . $bean->getUrlEncoded();
 	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
 }
+//  japan
+$res .= "<!-- Japan shelters listing   -->   \n";
+$svc = new SheltersJapanSvcImpl();
+$beans=$svc->selTodos(null, null, 0, 0, null, null, 0, 100000);
+foreach ($beans as $bean){
+	$url= $rootUrl . "shelters/info/japan/" . $bean->getUrlEncoded();
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+//  canada
+$res .= "<!-- Canada shelters listing   -->   \n";
+$svc = new SheltersCanadaSvcImpl();
+$beans=$svc->selTodos(null, null, 0, 0, null, null, 0, 100000);
+foreach ($beans as $bean){
+	$url= $rootUrl . "shelters/info/canada/" . $bean->getUrlEncoded();
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+//  uk
+$res .= "<!-- UK shelters listing   -->   \n";
+$svc = new SheltersUkSvcImpl();
+$beans=$svc->selTodos(null, null, null, 0, 0, null, null, 0, 100000);
+foreach ($beans as $bean){
+	$url= $rootUrl . "shelters/info/uk/" . $bean->getUrlEncoded();
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+//  india
+$res .= "<!-- india shelters listing   -->   \n";
+$svc = new SheltersIndiaSvcImpl();
+$beans=$svc->selTodos(null, null, 0, 0, null, null, 0, 100000);
+foreach ($beans as $bean){
+	$url= $rootUrl . "shelters/info/india/" . $bean->getUrlEncoded();
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+//  China
+$res .= "<!-- China shelters listing   -->   \n";
+$svc = new SheltersChinaSvcImpl();
+$beans=$svc->selTodos(null, null, 0, 0, null, null, 0, 100000);
+foreach ($beans as $bean){
+	$url= $rootUrl . "shelters/info/china/" . $bean->getUrlEncoded();
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+
+//páginas individuales de razas caninas
+$svc = new DogBreedsSvcImpl();
+$beans = $svc->selecciona(null, null, null, null, null, null, null, null,  null, 0, 100000);
+$res .= "<!-- dog breeds listing   -->   \n";
+foreach ($beans as $bean){
+	$url= $rootUrl . "dogbreeds/info/" . str_replace(" ", "_", $bean->getNombre() ) ;
+	$res .= construyeUnidad($url, $lastMod, "monthly", 0.5);
+}
+
+
 
 $res .="</urlset>";
 
