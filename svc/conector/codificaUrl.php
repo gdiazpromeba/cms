d1335eec6f24f802aa305ede0285d1c2393a2fd5
Gonzalo Díaz <gdiaz@starmount.com>
2014-12-15 01:17:06 -0300

    el servicio de codificación se normaliza

--- svc/conector/codificaUrl.php
+++ svc/conector/codificaUrl.php
@@ -1,7 +1,7 @@
 <?php
   require_once '../../config.php';
   header("Content-Type: text/plain; charset=utf-8");
-  require_once $_SERVER['DOCUMENT_ROOT'] . $GLOBALS['dirWeb'] . '/utils/UrlUtils.php';
+  require_once $GLOBALS['pathCms'] . '/util/UrlUtils.php';
   
 //   require_once('FirePHPCore/fb.php4'); 
   
